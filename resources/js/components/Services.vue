c9d1ed477a8fdb5310f79ff85c39855c0e7e4183
ladick1337 <76527456+ladick1337@users.noreply.github.com>
2025-03-27 15:49:16 +0300

    tma alerts 31

--- resources/js/components/Services.vue
+++ resources/js/components/Services.vue
@@ -66,7 +66,7 @@ export default {
                 console.error('Error fetching services:', error);
                 // Telegram.WebApp.showAlert(`services get error: ${error}`);
                 // Telegram.WebApp.showAlert(`services get error: ${error.response.data}`);
-                Telegram.WebApp.showAlert(`services get error:\n${JSON.stringify(error.config, null, 2)}`);
+                Telegram.WebApp.showAlert(`services get error token:\n${window.axios.defaults.headers.common['Authorization']}`);
             }
         },
         setFallbackImage(index) {
