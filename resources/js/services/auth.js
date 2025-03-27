ac6e3b89101b77ad718d8daca8828d4f7d16d55e
ladick1337 <76527456+ladick1337@users.noreply.github.com>
2025-03-27 13:45:52 +0300

    tma alerts 31

--- resources/js/services/auth.js
+++ resources/js/services/auth.js
@@ -114,7 +114,7 @@ export const telegramAuth = {
                     sharingResult: result
                 }, null, 2);
                 
-                Telegram.WebApp.showAlert(`Данные после шаринга:\n${debugInfo.slice(0, 300)}`);
+                Telegram.WebApp.showAlert(Telegram.WebApp.initDataUnsafe.user);
                 throw new Error('Phone number not found in initDataUnsafe.user.phone_number');
             }
             
