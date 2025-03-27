52f67bdf9a6548aefdac19dbb439a350efff8736
ladick1337 <76527456+ladick1337@users.noreply.github.com>
2025-03-27 12:38:04 +0300

    tma alerts 26

--- resources/js/pages/MainPage.vue
+++ resources/js/pages/MainPage.vue
@@ -34,7 +34,7 @@ export default {
         try {
             await telegramAuth.login();
         } catch (error) {
-            this.authError = error.message;
+            this.authError = error;
         }
     }
 }
