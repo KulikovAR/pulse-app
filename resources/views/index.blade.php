e8e3bcce07098ddc19a71bd66369d1aee4fd7938
ladick1337 <76527456+ladick1337@users.noreply.github.com>
2025-04-03 16:40:17 +0300

    container fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, viewport-fit=cover">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, viewport-fit=cover" >
     <link rel="stylesheet" href="/css/normalize.css">
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="/css/variables.css">
@@ -12,13 +12,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,500&display=swap" rel="stylesheet">
 
     <script src="https://telegram.org/js/telegram-web-app.js"></script>
-    <!-- <script async src="https://telegram.org/js/telegram-widget.js?21" 
-        data-telegram-login="PulseAppNotifyBot" 
-        data-size="large" 
-        data-radius="10"
-        data-auth-url="https://pulse-back.pisateli-studio.ru/api/v1/telegram/login"
-        data-request-access="write">
-    </script> -->
 
     <title>Pulse</title>
     
