b183c8a2fa84b7120630d2ef27249071d5e27613
ladick1337 <76527456+ladick1337@users.noreply.github.com>
2025-04-21 11:31:43 +0300

    tg close btn

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -5,13 +5,31 @@
 <script>
 export default {
     name: 'App',
+    watch: {
+        '$route'(to) {
+            const telegram = window.Telegram.WebApp;
+            
+            // Show Back button on all pages except main
+            if (to.name === 'main') {
+                telegram.BackButton.hide();
+                telegram.ClosingConfirmation = true;
+            } else {
+                telegram.ClosingConfirmation = false;
+                telegram.BackButton.show();
+            }
+        }
+    },
     mounted() {
         const telegram = window.Telegram.WebApp;
         telegram.ready();
 
-        // Show both back and close buttons
-        telegram.BackButton.show();
-        telegram.ClosingConfirmation = true;
+        // Initial state based on current route
+        if (this.$route.name === 'main') {
+            telegram.BackButton.hide();
+            telegram.ClosingConfirmation = true;
+        } else {
+            telegram.BackButton.show();
+        }
 
         // Back button handler
         telegram.BackButton.onClick(() => {
